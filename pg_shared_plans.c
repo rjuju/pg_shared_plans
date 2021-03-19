11625c25584569603b1f93d4b3f59f08ab08328a
Julien Rouhaud <julien.rouhaud@free.fr>
2021-03-19 12:30:13 +0800

    Reduce locking overhead.

--- pg_shared_plans.c
+++ pg_shared_plans.c
@@ -46,8 +46,6 @@ typedef struct pgspHashKey
 typedef struct pgspEntry
 {
 	pgspHashKey key;			/* hash key of entry - MUST BE FIRST */
-	slock_t		mutex;			/* protects the plan */
-	dsa_pointer plan;
 	dsm_handle  h;
 } pgspEntry;
 
@@ -309,7 +307,6 @@ pgsp_planner_hook(Query *parse,
 	PlannedStmt	   *result, *generic;
 	pgspHashKey		key;
 	pgspEntry	   *entry;
-	dsm_handle h = 0;
 
 	if (!pgsp_enabled)
 		goto fallback;
@@ -337,46 +334,34 @@ pgsp_planner_hook(Query *parse,
 	LWLockAcquire(pgsp->lock, LW_SHARED);
 	entry = (pgspEntry *) hash_search(pgsp_hash, &key, HASH_FIND, NULL);
 
-	if (entry != NULL)
+	if (entry && entry->h != DSM_HANDLE_INVALID)
 	{
-		volatile pgspEntry *e = (volatile pgspEntry *) entry;
-
-		/*
-		 * Grab the spinlock to get the handle
-		 */
-		SpinLockAcquire(&e->mutex);
-		h = entry->h;
-		SpinLockRelease(&e->mutex);
-	}
-
-	if (h != DSM_HANDLE_INVALID)
-	{
-		dsm_segment *eseg;
-		shm_toc *etoc;
+		dsm_segment *seg;
+		shm_toc *toc;
 		char *local;
 
-		eseg = dsm_attach(h);
-		if (eseg == NULL)
+		seg = dsm_attach(entry->h);
+		if (seg == NULL)
 		{
 			LWLockRelease(pgsp->lock);
 			goto fallback;
 		}
 
-		etoc = shm_toc_attach(PGSP_MAGIC, dsm_segment_address(eseg));
+		toc = shm_toc_attach(PGSP_MAGIC, dsm_segment_address(seg));
 
-		local = (char *) shm_toc_lookup(etoc, 0, true);
+		local = (char *) shm_toc_lookup(toc, 0, true);
 
 		if (local != NULL)
 		{
 			elog(NOTICE, "found entry, bypassing planner");
 
 			result = (PlannedStmt *) stringToNode(local);
-			dsm_detach(eseg);
+			dsm_detach(seg);
 
 			goto release;
 		}
 
-		dsm_detach(eseg);
+		dsm_detach(seg);
 	}
 	else
 		elog(PGSP_LEVEL, "entry not found :(");
@@ -395,14 +380,42 @@ pgsp_planner_hook(Query *parse,
 	/* Save the plan if no one did it yet */
 	if (!entry)
 	{
-		volatile pgspEntry *e;
-		dsm_segment *eseg = NULL;
+		shm_toc_estimator estimator;
+		dsm_segment *seg = NULL;
+		dsm_handle h;
+		shm_toc *toc;
+		char *local;
 		char *serialized;
 		size_t len, size;
-		shm_toc_estimator estimator;
 
+		/*
+		 * We store the plan is dsm before acquiring the lwlock.  It means that
+		 * we may have to discard it, but it avoids locking overhead.
+		 */
 		serialized = nodeToString(generic);
 		len = strlen(serialized) + 1;
+
+		shm_toc_initialize_estimator(&estimator);
+		shm_toc_estimate_keys(&estimator, 1);
+		shm_toc_estimate_chunk(&estimator, len);
+		shm_toc_estimate_chunk(&estimator, 200);
+		size = shm_toc_estimate(&estimator);
+
+		seg = dsm_create(add_size(size, 2000), DSM_CREATE_NULL_IF_MAXSEGMENTS);
+
+		/* out of memory */
+		if (seg == NULL)
+		{
+			elog(PGSP_LEVEL, "out of mem");
+			goto finish;
+		}
+
+		h = dsm_segment_handle(seg);
+		toc = shm_toc_create(PGSP_MAGIC, dsm_segment_address(seg), size);
+		size -= 200;
+		local = (char *) shm_toc_allocate(toc, size);
+		memcpy(local, serialized, len);
+		shm_toc_insert(toc, 0, local);
 
 		LWLockAcquire(pgsp->lock, LW_EXCLUSIVE);
 		entry = pgsp_entry_alloc(&key);
@@ -416,38 +429,15 @@ pgsp_planner_hook(Query *parse,
 			goto release;
 		}
 
-		e = (volatile pgspEntry *) entry;
-
-		shm_toc_initialize_estimator(&estimator);
-		shm_toc_estimate_keys(&estimator, 1);
-		shm_toc_estimate_chunk(&estimator, len);
-		shm_toc_estimate_chunk(&estimator, 200);
-		size = shm_toc_estimate(&estimator);
-
-		if (e->h == DSM_HANDLE_INVALID)
+		/* Store the segment if we just allocated the entry */
+		if (entry->h == DSM_HANDLE_INVALID)
 		{
-			shm_toc *etoc;
-			char *local;
+			Assert(h != DSM_HANDLE_INVALID);
+			entry->h = h;
+			dsm_pin_segment(seg);
+			on_dsm_detach(seg, pgsp_detach, (Datum) 0);
 
-			eseg = dsm_create(add_size(size, 2000), DSM_CREATE_NULL_IF_MAXSEGMENTS);
-	
-			/* out of memory */
-			if (eseg == NULL)
-			{
-				elog(PGSP_LEVEL, "out of mem");
-				goto release;
-			}
-
-			dsm_pin_segment(eseg);
-			on_dsm_detach(eseg, pgsp_detach, (Datum) 0);
-
-			e->h = dsm_segment_handle(eseg);
-			etoc = shm_toc_create(PGSP_MAGIC, dsm_segment_address(eseg), size);
-			size -= 200;
-			local = (char *) shm_toc_allocate(etoc, size);
-			memcpy(local, serialized, len);
-			shm_toc_insert(etoc, 0, local);
-			dsm_detach(eseg);
+			dsm_detach(seg);
 		}
 		goto release;
 	}
@@ -544,10 +534,7 @@ pgsp_entry_alloc(pgspHashKey *key)
 	if (!found)
 	{
 		/* New entry, initialize it */
-		entry->plan = 0;
 		entry->h = 0;
-		/* re-initialize the mutex each time ... we assume no one using it */
-		SpinLockInit(&entry->mutex);
 	}
 
 	return entry;
